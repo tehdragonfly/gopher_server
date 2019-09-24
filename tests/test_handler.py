ab58dceb3a5f180d20844713766b6b2551400ff1
Olly Parton <mysticdragonfly@hotmail.co.uk>
2019-09-24 23:16:31 +0100

    Read example files for comparison.

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -4,21 +4,25 @@ import pytest
 from gopher_server.handlers import DirectoryHandler
 
 
+BASE_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "examples/data/")
+
+
 @pytest.fixture
 def directory_handler():
-    root = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "examples/data/")
-    return DirectoryHandler(root)
+    return DirectoryHandler(BASE_PATH)
 
 
 @pytest.mark.asyncio
 async def test_directory_handler_file(directory_handler: DirectoryHandler):
     """File path returns file from the directory."""
     response = await directory_handler.handle("example")
-    assert response == "hello world example document\n"
+    with open(os.path.join(BASE_PATH + "example")) as f:
+        assert response == f.read()
 
 
 @pytest.mark.asyncio
 async def test_directory_handler_directory(directory_handler: DirectoryHandler):
     """Directory name returns index file from the directory."""
     response = await directory_handler.handle("")
-    assert response == "iHello world!\t\terror.host\t0\n0example document\t/example\tlocalhost\t7000\n"
+    with open(os.path.join(BASE_PATH + "index")) as f:
+        assert response == f.read()
