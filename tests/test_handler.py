44c057f44a58f4f9251e00e321ededaa433edab5
Olly Parton <mysticdragonfly@hotmail.co.uk>
2019-09-25 21:04:05 +0100

    Add PatternHandler tests.

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,14 +1,14 @@
 import os.path
 import pytest
 
-from gopher_server.handlers import DirectoryHandler
+from gopher_server.handlers import DirectoryHandler, NotFound, PatternHandler
 
 
 BASE_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "examples/data/")
 
 
 @pytest.fixture
-def directory_handler():
+def directory_handler() -> DirectoryHandler:
     return DirectoryHandler(BASE_PATH)
 
 
@@ -26,3 +26,49 @@ async def test_directory_handler_directory(directory_handler: DirectoryHandler):
     response = await directory_handler.handle("")
     with open(os.path.join(BASE_PATH + "index")) as f:
         assert response == f.read()
+
+
+@pytest.mark.asyncio
+async def test_directory_handler_not_found(directory_handler: DirectoryHandler):
+    """Non-existient file raises NotFound."""
+    with pytest.raises(NotFound):
+        await directory_handler.handle("qwertyuiop")
+
+
+@pytest.fixture
+def pattern_handler() -> PatternHandler:
+    handler = PatternHandler()
+
+    @handler.register("static_pattern")
+    def static_pattern(selector):
+        """Static pattern function just receives the selector."""
+        assert selector == "static_pattern"
+        return "static pattern response"
+
+    @handler.register("dynamic_pattern/(?P<arg>.+)")
+    def dynamic_pattern(selector, not_arg=None, arg=None):
+        """Dynamic pattern function receives named capturing groups as keyword arguments."""
+        assert selector == "dynamic_pattern/" + arg
+        assert not_arg is None
+        return "dynamic response for " + arg
+
+    return handler
+
+
+@pytest.mark.asyncio
+async def test_pattern_handler_static(pattern_handler: PatternHandler):
+    response = await pattern_handler.handle("static_pattern")
+    assert response == "static pattern response"
+
+
+@pytest.mark.asyncio
+async def test_pattern_handler_dynamic(pattern_handler: PatternHandler):
+    response = await pattern_handler.handle("dynamic_pattern/foo")
+    assert response == "dynamic response for foo"
+
+
+@pytest.mark.asyncio
+async def test_pattern_handler_not_found(pattern_handler: PatternHandler):
+    """Unrecognised pattern raises NotFound."""
+    with pytest.raises(NotFound):
+        await pattern_handler.handle("qwertyuiop")
