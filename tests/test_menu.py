06663fcaef1b9e6943799b641c0868eaf429d7d9
Olly Parton <mysticdragonfly@hotmail.co.uk>
2019-09-26 23:50:50 +0100

    Add Menu tests.

---
+++ tests/test_menu.py
@@ -0,0 +1,19 @@
+from gopher_server.menu import InfoMenuItem, Menu, MenuItem
+
+
+def test_menu_item():
+    menu_item = MenuItem("0", "foo", "hello/foo", "localhost", 7000)
+    assert menu_item.serialize() == "0foo\thello/foo\tlocalhost\t7000"
+
+
+def test_info_menu_item():
+    menu_item = InfoMenuItem("hello world example menu")
+    assert menu_item.serialize() == "ihello world example menu\t\terror.host\t0"
+
+
+def test_menu():
+    menu = Menu([
+        InfoMenuItem("hello world example menu"),
+        MenuItem("0", "foo", "hello/foo", "localhost", 7000),
+    ])
+    assert menu.serialize() == "ihello world example menu\t\terror.host\t0\r\n0foo\thello/foo\tlocalhost\t7000"
