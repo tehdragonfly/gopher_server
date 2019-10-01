5d68afb2638a14c70398ec9d026440fe834068f7
Olly Parton <mysticdragonfly@hotmail.co.uk>
2019-10-01 23:52:52 +0100

    Add more handler docs.

--- gopher_server/handlers.py
+++ gopher_server/handlers.py
@@ -31,7 +31,12 @@ class IHandler(Interface):
 
 @implementer(IHandler)
 class DirectoryHandler:
-    """Serves files from a directory, as specified by `base_path`."""
+    """
+    Serves files from a directory, as specified by `base_path`.
+
+    .. note:: If the selector matches the name of a directory, this will look
+              for a file called `index` in that directory.
+    """
 
     def __init__(self, base_path: str):
         self.base_path = os.path.abspath(base_path)
@@ -48,19 +53,46 @@ class DirectoryHandler:
         if not file_path.startswith(self.base_path):
             raise NotFound
 
+        # TODO directory listing
         if os.path.isdir(file_path):
             file_path = os.path.join(file_path, "index")
 
         if not os.path.isfile(file_path):
             raise NotFound
 
+        # TODO read binary files as bytes
         with open(file_path) as f:
             return f.read()
 
 
 @implementer(IHandler)
 class PatternHandler:
-    """Uses pattern matching to map selectors to view functions."""
+    """
+    Uses pattern matching to map selectors to view functions.
+
+    View functions can be registered by decorating them with
+    :func:`PatternHandler.register`. Requests with a selector matching the
+    pattern will then call that function. For example:
+
+    .. code-block::
+
+       @handler.register("hello/.+")
+       def hello(selector):
+           return "hello %s" % selector[6:]
+
+    Named capturing groups will be passed to the function as keyword
+    arguments:
+
+    .. code-block::
+
+       @handler.register("hello/(?P<name>.+)")
+       def hello(selector, name):
+           return "hello %s" % name
+
+    .. note:: Patterns are compared in the order in which they were
+              registered, so if the selector matches multiple patterns then
+              the one which was registered first will 'win'.
+    """
 
     def __init__(self):
         self.patterns = []
@@ -73,13 +105,7 @@ class PatternHandler:
         raise NotFound
 
     def register(self, pattern: str):
-        """
-        Decorator to register a view function.
-
-        .. note:: Patterns are compared in the order in which they were
-                  registered, so if a selector matches multiple patterns then
-                  the one which was registered first will 'win'.
-        """
+        """Decorator to register a view function."""
 
         pattern = re.compile("^%s$" % pattern)
 
