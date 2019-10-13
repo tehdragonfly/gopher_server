a5bd99712223ba2acacf4edd6ed85fc82bb8c53d
Olly Parton <mysticdragonfly@hotmail.co.uk>
2019-10-13 23:19:45 +0100

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,27 +1,104 @@
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from setuptools import setup
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = "gopher_server"
+DESCRIPTION = "Library for building Gopher servers."
+URL = "https://github.com/tehdragonfly/gopher_server"
+EMAIL = "mysticdragonfly@hotmail.co.uk"
+AUTHOR = "Olly Parton"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "0.1.0"
+
+INSTALL_REQUIRES = ["aioquic", "cryptography", "zope.interface"]
+
+TESTS_REQUIRE = ["pytest", "pytest-asyncio"]
+
+EXTRAS = {}
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md')) as f:
-    README = f.read()
 
-CHANGES = ""
+# Import the README and use it as the long-description.
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
 
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(VERSION))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name="gopher_server",
-    version="0.0",
-    description="gopher_server",
-    long_description=README,
-    packages=["gopher_server"],
-    zip_safe=False,
-    install_requires=[
-        "aioquic",
-        "cryptography",
-        "zope.interface",
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    py_modules=["gopher_server"],
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy"
     ],
-    tests_require=[
-        "pytest",
-        "pytest-asyncio",
-    ],
+    # $ setup.py publish support.
+    cmdclass={
+        "upload": UploadCommand,
+    },
 )
+
