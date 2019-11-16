0b1a5b445e4a209a3e300806f51c9753a468b25b
Olly Parton <mysticdragonfly@hotmail.co.uk>
2019-11-16 23:57:22 +0000

    Test NotFound catching.

--- tests/test_application.py
+++ tests/test_application.py
@@ -4,14 +4,12 @@ from typing import Union
 from zope.interface import implementer
 
 from gopher_server.application import Application
-from gopher_server.handlers import IHandler, Request
+from gopher_server.handlers import IHandler, NotFound, Request
 
 
 @implementer(IHandler)
 class TestHandler:
     async def handle(self, request: Request) -> Union[str, bytes]:
-        assert request.selector in ("string", "bytes", "exception")
-
         if request.selector == "string":
             return "test"
 
@@ -20,6 +18,8 @@ class TestHandler:
 
         if request.selector == "exception":
             raise Exception
+
+        raise NotFound
 
 
 @pytest.fixture
@@ -62,7 +62,14 @@ async def test_not_utf8(application: Application):
 
 
 @pytest.mark.asyncio
+async def test_not_found(application: Application):
+    """NotFound exceptions should serve a not found error."""
+    response = await application.dispatch("localhost", 7000, b"notfound\r\n")
+    assert response == b"3Not found.\t\terror.host\t0\r\n.\r\n"
+
+
+@pytest.mark.asyncio
 async def test_exception(application: Application):
-    """Exceptions from the handler should be eaten."""
+    """Other exceptions from the handler should be eaten."""
     response = await application.dispatch("localhost", 7000, b"exception\r\n")
     assert response == b"3Internal server error.\t\terror.host\t0\r\n.\r\n"
